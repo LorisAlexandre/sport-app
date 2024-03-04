fbb79616795fade7562ac00ef9f06c7dc2c7edc7
Loris ALEXANDRE <loris.alexandre66@gmail.com>
2024-03-04 20:04:32 +0100

    test if await auth crash or prisma

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
+import { auth } from "@/lib/auth";
 import { BuyButton, LoginButton } from "./ui";
 
 const Header = async () => {
+  const session = await auth();
+
   return (
     <div>
       <h1>Header</h1>
+      {session?.user && <p>{session.user.email}</p>}
       <BuyButton />
       <LoginButton />
     </div>
