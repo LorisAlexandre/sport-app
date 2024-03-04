d7e73c5b563082a4ac395a0fa047d4e92e1675fd
Loris ALEXANDRE <loris.alexandre66@gmail.com>
2024-03-04 19:25:41 +0100

    test

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,12 @@ import { findUserByAuthSession } from "@/lib/db";
 
 const Header = async () => {
   // const session = await auth();
-  const user = await findUserByAuthSession();
+  // const user = await findUserByAuthSession();
 
   return (
     <div>
       <h1>Header</h1>
-      <BuyButton customerId={user?.stripeCustomerId ?? ""} />
+      {/* <BuyButton customerId={user?.stripeCustomerId ?? ""} /> */}
       <LoginButton />
       {/* {session && <p>{session.user?.email}</p>} */}
     </div>
