4aad86fa8e95fa9937d1fd7e908b273e48e56c2c
Loris ALEXANDRE <loris.alexandre66@gmail.com>
2024-03-12 17:44:31 +0100

    retrieve checkout items

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,25 +1,83 @@
 import { findUserFromCustomer, prisma } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
 import { User } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+
+/**
+ * 
+ * {
+  id: 'cs_test_a1JyJp1w63lbR42vhMddpS1SelzXQXUwHcrjrxfXdFEIb0smgXz1RpjGVu',
+  object: 'checkout.session',
+  after_expiration: null,
+  allow_promotion_codes: null,
+  amount_subtotal: 10000,
+  amount_total: 5000,
+  automatic_tax: { enabled: false, liability: null, status: null },
+  billing_address_collection: null,
+  cancel_url: 'https://sport-app-zeta.vercel.app/',
+  client_reference_id: null,
+  client_secret: null,
+  consent: null,
+  consent_collection: null,
+  created: 1710261219,
+  currency: 'eur',
+  currency_conversion: null,
+  custom_fields: [],
+  custom_text: {
+    after_submit: null,
+    shipping_address: null,
+    submit: null,
+    terms_of_service_acceptance: null
+  },
+  customer: 'cus_Pf9ZuI3uw53MeJ',
+  payment_intent: 'pi_3OtYH6BvdD53Amib0Irb2bAs',
+  payment_link: null,
+  payment_method_collection: 'if_required',
+  payment_method_configuration_details: null,
+  payment_method_options: { card: { request_three_d_secure: 'automatic' } },
+  payment_method_types: [ 'card' ],
+  payment_status: 'paid',
+  phone_number_collection: { enabled: false },
+  recovered_from: null,
+  setup_intent: null,
+  shipping_address_collection: null,
+  shipping_cost: null,
+  shipping_details: null,
+  shipping_options: [],
+  status: 'complete',
+  submit_type: null,
+  subscription: null,
+  success_url: 'https://sport-app-zeta.vercel.app/account',
+  total_details: { amount_discount: 5000, amount_shipping: 0, amount_tax: 0 },
+  ui_mode: 'hosted',
+  url: null
+}
+ * 
+ * 
+ */
 
 export const POST = async (req: NextRequest) => {
   const body = (await req.json()) as Stripe.Event;
 
   switch (body.type) {
     case "checkout.session.completed": {
-      const session = body.data.object as Stripe.Checkout.Session;
-      const stripeCustomerId = session.customer as string;
+      const checkoutSession = body.data.object as Stripe.Checkout.Session;
+      const stripeCustomerId = checkoutSession.customer as string;
+      const stripeSession = await stripe.checkout.sessions.retrieve(
+        checkoutSession.id
+      );
       const user = await findUserFromCustomer(stripeCustomerId);
 
-      console.log(session);
       if (!user) {
         break;
       }
 
       let plan: User["plan"] = "Premium";
 
-      if (session.line_items?.data[0].id === process.env.COACH_STRIPE_ID) {
+      if (
+        stripeSession.line_items?.data[0].id === process.env.COACH_STRIPE_ID
+      ) {
         plan = "Coach";
       }
 
