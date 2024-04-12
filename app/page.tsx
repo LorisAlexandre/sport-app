6ce0b2b1419fbab1e4877af040ece008418c5be4
Loris ALEXANDRE <loris.alexandre66@gmail.com>
2024-04-12 18:33:36 +0200

    add video

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
       </nav>
       <Herobanner />
       <Customers />
-      {/* <Demo /> */}
+      <Demo />
       {/* <Proof /> */}
       <FeaturedOn />
       <WhyBuy />
--- components/HomePage/Demo.tsx
+++ components/HomePage/Demo.tsx
@@ -2,9 +2,9 @@ export const Demo = () => {
   return (
     <div className="flex justify-center">
       <iframe
-        className="w-full max-w-[560px] h-[315px]"
-        src=""
-        title="YouTube video player, Propulze demo try it now!"
+        className="w-full max-w-[450px] h-[560px]"
+        src="https://www.loom.com/embed/052433bfaf8f4e8180999c65ba419cdc?sid=37ee812d-1da1-427f-8ed8-af1adfcb3082"
+        title="Propulze demo try it now!"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
